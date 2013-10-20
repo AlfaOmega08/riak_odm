c20a2affc784c6773363051ff1d0b03756f200ae
Francesco Boffa <fra.boffa@gmail.com>
2013-10-20 23:38:15 +0200

    Refactored destroyable.

--- lib/riak_odm/destroyable.rb
+++ lib/riak_odm/destroyable.rb
@@ -24,10 +24,7 @@ module RiakOdm
       # This method is potentially very slow.
       def destroy_all(options = {})
         keys = self.bucket.all_keys
-        keys.each do |key|
-          doc = self.class.find(key)
-          doc.destroy
-        end
+        keys.each { |key| destroy(key, options) }
       end
 
       def delete(key, options = {})
@@ -38,9 +35,7 @@ module RiakOdm
       # This still might be very slow.
       def delete_all(options = {})
         keys = self.bucket.all_keys
-        keys.each do |key|
-          self.class.delete(key, options)
-        end
+        keys.each { |key| self.class.delete(key, options) }
       end
     end
   end
